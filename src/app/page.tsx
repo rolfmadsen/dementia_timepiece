af45bfdab8dd1bf6162f0c03d7e61a2cf817fc6e
rolfmadsen <96595+rolfmadsen@users.noreply.github.com>
2023-10-18 11:28:32 +0200

    Removed Privacy link on frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-//'use client';
-
 import React from 'react';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
@@ -10,9 +8,11 @@ export default function App() {
   return (
     <div>
       <TimeIndicator />
+      {/* 
       <p className="text-[clamp(0.8rem,2vw,1.2rem)]">
         <Link className="absolute bottom-0 left-0 m-4 opacity-80" href="/privacy_policy">Privatlivspolitik</Link>
       </p>
+    */}
     </div>
   );
 };
